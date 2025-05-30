ce36de3036decca5c825620a10d9eac93cdf2a0f
Octavio Arnaudo <octavioarnaudo@hotmail.com>
2025-05-29 21:01:46 -0300

    21:01 29-05-25

--- build.bat
+++ build.bat
@@ -5,6 +5,7 @@ setlocal
 
 set "REPO_URL=https://github.com/OctavioArnaudo/Absolutivo.git"
 set "CLONE_DIR=MyProject"
+set "UNITY_EDITOR_PATH=C:\Program Files\Unity\Hub\Editor\6000.0.45f1\Editor\Unity.exe"
 git clone %REPO_DIR% %CLONE_DIR%
 if %ERRORLEVEL% NEQ 0 (
     echo.
@@ -12,6 +13,22 @@ if %ERRORLEVEL% NEQ 0 (
     echo.
     pause
     goto :end
+)
+
+
+
+cd "%CLONE_DIR%"
+"%UNITY_EDITOR_PATH%" -projectPath "%CD%"
+if %ERRORLEVEL% NEQ 0 (
+    echo.
+    echo ADVERTENCIA: Fallo al abrir Unity Editor.
+    echo.
+    rem No es un error critico, seguimos con la limpieza.
+) else (
+    echo.
+    echo Unity Editor iniciado. Por favor, trabaja en tu proyecto.
+    echo Cuando termines, guarda y cierra Unity Editor para continuar con la limpieza.
+    pause
 )
 
 
